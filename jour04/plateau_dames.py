c93ca099b19e6fc57ea415a4d76155989291a34a
eltarr-kawther <kawther.eltarr@laplateforme.io>
2020-10-19 14:19:54 +0200

    upload progress checkboard

--- jour04/plateau_dames.py
+++ jour04/plateau_dames.py
@@ -11,14 +11,26 @@ def checkerboard(n):
     board = np.zeros((n,n))
     
     #p = random.randint(0, n-1)
-    #board[0, p] = board[0, p] + 1
-    
-    for i in range(0, n):
+    board[0, 4] = board[0, 4] + 1
+    print(board)
+    for i in range(1, n):
         for j in range(0, n):
-            print()
-            #board[i, p] = board[i, p] + 1    
+            print('Ligne {}, colonne {}'.format(i,j))
+            if (j < n - 1) and (j !=0):
+                if (board[i-1, j-1]!=1) and (board[i-1, j]!=1) and (board[i-1, j+1]!=1):
+                    print("Emplacement de dame possible")
+                    print()
+                    #board[i, p] = board[i, p] + 1   
+            elif (j==n-1):
+                if (board[i-1, j-1]!=1) and (board[i-1, j]!=1):
+                    print("Emplacement de dame possible")
+                    print()
+            elif (j==0):
+                if (board[i-1, j]!=1) and (board[i-1, j+1]!=1):
+                    print("Emplacement de dame possible")
+                    print()
     return board
 
-checkerboard(10)
+checkerboard(8)
 
 
